74fc9314c2e6f108dcbd3b5b905f5ad78d209fa2
luigielbambino <luis_al82@hotmail.com>
2018-10-24 19:01:28 +0200

    Changes to pass Test 1 and 2. Set the byte values correctly in the array positions and build the array properly with 0x00 initial values.

--- Image.java
+++ Image.java
@@ -1,47 +1,65 @@
-package exercise1;
 
 public class Image {
-  public byte[] data;
-  private int width;
-  private int height;
+    public byte[] data;
+    private int width;
+    private int height;
 
-  public Image(int width, int height){
-    //Create the aray with the length according to the given Width and Height
-	this.width = width;
-	this.height = height;
-	int length = width * height * 3;
-    data = new byte[length];
+    //Constructor of the image object
+    public Image(int width, int height){
+        //Create the array with the length according to the given Width and Height
+        this.width = width;
+        this.height = height;
+        int arraySize = width * height * 3;
+        data = new byte[arraySize];
 
-    //Set the bytes of the array to 00
-    for(int i = 0; i <= data.length; i++){
-        data[i]=(byte)0x00;
-    }
-  }
-
-  public void set(int x, int y, int val){
-    int index;
-    if(x == 0 && y == 0){
-      index = 0;
-    } else if(y == 0 && x != 0){
-      index = x * 3;
-    } else if(x == 0 && y != 0){
-      index = x * (this.width * 3);
-    } else{
-      index = x * y;
+        //Set the values of the byte  array to 00
+        for(int i = 0; i < data.length; i++){
+            data[i]=(byte)0x00;
+        }
     }
 
-    String hexaValue = Integer.toString(val);
-    char[] charArray = hexaValue.toCharArray();
-    int r = Character.getNumericValue(charArray[0] + charArray[1]);
-    int g = Character.getNumericValue(charArray[2] + charArray[3]);
-    int b = Character.getNumericValue(charArray[4] + charArray[5]);
+    //Create getters
+    public byte[] getData(){
+      return this.data;
+    }
+    public int getWidth(){
+        return this.width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
 
-    data[index] = (byte)r;
-    data[index+1] = (byte)g;
-    data[index+2] = (byte)b;
-  }
+    //Create setters
+    public void setData(byte[] updateData){
+        this.data = updateData;
+    }
+    public void setWidth(int newWidth){
+        this.width = newWidth;
+    }
+    public void setHeight(int newHeight){
+        this.height = newHeight;
+    }
 
-  public void write(String filename){
-  }
+    //Set method to insert values into the byte array
+    //Make exceptions when the coordinates are too low to calculate regularly
+    public void set(int x, int y, int val){
+        int index;
+        if(x == 0 && y == 0){
+          index = 0;
+        } else if(y == 0 && x != 0){
+          index = x * 3;
+        } else if(x == 0 && y != 0){
+          index = x * (this.width * 3);
+        } else{
+          index = x * y;
+        }
+
+        //Set the values of the array by the given int value(val) shifting 8 bits from val value
+        data[index] = (byte)(val >> 16);
+        data[index+1] = (byte)(val >> 8);
+        data[index+2] = (byte)(val >> 0);
+    }
+
+    public void write(String filename){}
 
 }
